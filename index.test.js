947e175a035cd1d95627c33e79f17f2c4e2fb1c8
Gabriel Brecci <gabriel.brecci@ltm.digital>
2020-10-29 18:17:11 -0300

    Gabriel pt2

--- index.test.js
+++ index.test.js
@@ -1,4 +1,5 @@
 const campoMinado = require('./index')
+const mockCampoMinado = campoMinado.campoMinado(4,4, '*........*......');
 
 test('Dado a entrada de campo 4 por 4, espero que a line seja 4', () => {
     expect(campoMinado.campoMinado(4,4, '*........*......').line).toBe(4);
@@ -9,6 +10,7 @@ test('Dado a entrada de campo 4 por 4, espero que a column seja 4', () => {
 });
 
 test('Dado a entrada *........*...... num campo 4 por 4, espero que a matrix seja construida corretamente', () => {
-    let campoMinado = campoMinado.campoMinado(4,4, '*........*......');
+    let campoMinado = campoMinado.render();
+    expect(campoMinado).toBe(mockCampoMinado);
     
 });
