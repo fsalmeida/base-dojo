26b7deee94e926891c3b4cd0e8d74e163cf3ba5e
Felipe da Silva Almeida <felipe.almeida@aberto.com.br>
2020-10-30 23:46:22 -0300

    Problema solucionado

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
+const BOMB_SYMBOL = '*';
+
 const buildField = (lines, columns, inputField) => {
+    let field = splitInputFieldIntoMatrixField(lines, columns, inputField);
+    identifyNumberOfBombsOnEachFieldElement(field);
+
+    return field;
+}
+
+const identifyNumberOfBombsOnEachFieldElement = (field) => {
+    for (let l = 0; l < field.length; l++) {
+        let currentLine = field[l];
+        for (let c = 0; c < currentLine.length; c++) {
+            let targerElement = currentLine[c];
+            let isBomb = targerElement == BOMB_SYMBOL;
+            if (isBomb)
+                continue;
+
+            let boundaries = findBoundaries(field, l, c);
+            let boundariesElements = boundaries.map(boundary => field[boundary.line][boundary.column]);
+            let nearbyBombsCount = boundariesElements.filter(elem => elem == BOMB_SYMBOL).length;
+            field[l][c] = nearbyBombsCount;
+        }
+    }
+}
+
+const findBoundaries = (field, targetLine, targetColumn) => {
+    const initialBoundaryLine = targetLine - 1;
+    const finalBoundaryLine = targetLine + 1;
+    const initialBoundaryColumn = targetColumn - 1;
+    const finalBoundaryColumn = targetColumn + 1;
+    let boundaries = [];
+
+    for (let currentLine = initialBoundaryLine; currentLine <= finalBoundaryLine; currentLine++) {
+        if (!validateLine(field, currentLine))
+            continue;
+
+        for (let currentColumn = initialBoundaryColumn; currentColumn <= finalBoundaryColumn; currentColumn++) {
+            if (!validateColumn(field, currentColumn))
+                continue;
+
+            let isTargetElement = currentLine == targetLine && currentColumn == targetColumn;
+            if (!isTargetElement) {
+                boundaries.push({ line: currentLine, column: currentColumn });
+            }
+        }
+    }
+
+    return boundaries;
+}
+
+const validateLine = (field, line) => {
+    return line >= 0 && line < field.length;
+}
+
+const validateColumn = (field, column) => {
+    let firstLine = field[0];
+    return column >= 0 && column < firstLine.length;
+}
+
+const splitInputFieldIntoMatrixField = (lines, columns, inputField) => {
     let fieldElements = inputField.split('');
     let field = [];
 
--- index.test.js
+++ index.test.js
@@ -1,22 +1,93 @@
 const { Minefield } = require('./index')
 
-test('Dado a entrada de campo 4 por 4, espero que a line seja 4', () => {
-    let minefield = new Minefield(4, 4, '*........*......');
-    expect(minefield.lines).toBe(4);
+describe('Testes relativos aos campos simples de um campo de 4 por 4', () => {
+    let minefield = null;
+    beforeEach(() => {
+        minefield = new Minefield(4, 4, '*........*......');
+    });
+
+    test('O número de linhas deve ser 4', () => {
+        expect(minefield.lines).toBe(4);
+    });
+
+    test('O número de colunas deve ser 4', () => {
+        expect(minefield.columns).toBe(4);
+    });
+
+    test('As bombas devem ser posicionadas em [0,0] e [2,1]', () => {
+        expect(minefield.field[0][0]).toBe('*');
+        expect(minefield.field[2][1]).toBe('*');
+    });
 });
 
-test('Dado a entrada de campo 4 por 4, espero que a column seja 4', () => {
-    let minefield = new Minefield(4, 4, '*........*......');
-    expect(minefield.columns).toBe(4);
-});
+describe('Testes relativos a identificação do número de bombas ao retor de cada elemento do campo', () => {
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [0][1] seja 1', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[0][1]).toBe(1);
+    });
 
-test('Dado a entrada *........*...... num campo 4 por 4, espero que a matrix seja construida corretamente', () => {
-    let minefield = new Minefield(4, 4, '*........*......');
-    expect(minefield.field[0][0]).toBe('*');
-    expect(minefield.field[2][1]).toBe('*');
-});
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [0][2] seja 0', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[0][2]).toBe(0);
+    });
 
-test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [0][1] seja 1', () => {
-    let minefield = new Minefield(4, 4, '*........*......');
-    expect(minefield.field[0][1]).toBe('1');
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [0][3] seja 0', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[0][3]).toBe(0);
+    });
+
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [1][0] seja 2', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[1][0]).toBe(2);
+    });
+
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [1][1] seja 2', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[1][1]).toBe(2);
+    });
+
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [1][2] seja 1', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[1][2]).toBe(1);
+    });
+
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [1][3] seja 0', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[1][3]).toBe(0);
+    });
+
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [2][0] seja 1', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[2][0]).toBe(1);
+    });
+
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [2][2] seja 1', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[2][2]).toBe(1);
+    });
+
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [2][3] seja 0', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[2][3]).toBe(0);
+    });
+
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [3][0] seja 1', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[3][0]).toBe(1);
+    });
+
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [3][1] seja 1', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[3][1]).toBe(1);
+    });
+
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [3][2] seja 1', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[3][2]).toBe(1);
+    });
+
+    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [3][3] seja 0', () => {
+        let minefield = new Minefield(4, 4, '*........*......');
+        expect(minefield.field[3][3]).toBe(0);
+    });
 });
