2b4d5a24593e19315b65aee8a85a0f2d5a7745de
Felipe Almeida <felipe.silvalmeida@gmail.com>
2020-11-02 19:16:10 -0300

    Ajuste no teste, removendo test.only

--- index.test.js
+++ index.test.js
@@ -21,72 +21,72 @@ describe('Testes relativos aos campos simples de um campo de 4 por 4', () => {
 });
 
 describe('Testes relativos a identificação do número de bombas ao retor de cada elemento do campo', () => {
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [0][1] seja 1', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [0][1] seja 1', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[0][1]).toBe(1);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [0][2] seja 0', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [0][2] seja 0', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[0][2]).toBe(0);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [0][3] seja 0', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [0][3] seja 0', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[0][3]).toBe(0);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [1][0] seja 2', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [1][0] seja 2', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[1][0]).toBe(2);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [1][1] seja 2', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [1][1] seja 2', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[1][1]).toBe(2);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [1][2] seja 1', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [1][2] seja 1', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[1][2]).toBe(1);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [1][3] seja 0', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [1][3] seja 0', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[1][3]).toBe(0);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [2][0] seja 1', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [2][0] seja 1', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[2][0]).toBe(1);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [2][2] seja 1', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [2][2] seja 1', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[2][2]).toBe(1);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [2][3] seja 0', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [2][3] seja 0', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[2][3]).toBe(0);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [3][0] seja 1', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [3][0] seja 1', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[3][0]).toBe(1);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [3][1] seja 1', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [3][1] seja 1', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[3][1]).toBe(1);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [3][2] seja 1', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [3][2] seja 1', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[3][2]).toBe(1);
     });
 
-    test.only('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [3][3] seja 0', () => {
+    test('Dado a entrada *........*...... num campo 4 por 4, espero que o elemento [3][3] seja 0', () => {
         let minefield = new Minefield(4, 4, '*........*......');
         expect(minefield.field[3][3]).toBe(0);
     });
